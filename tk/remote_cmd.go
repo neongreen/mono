a95fbc452130e84db89191b1d6703f57d2cd65f2
Emily April Kazak <emily@artyom.me>
2025-10-30 00:15:56 +0100

    tk remote rm

--- tk/remote_cmd.go
+++ tk/remote_cmd.go
@@ -99,7 +99,44 @@ var remoteLsCmd = &cobra.Command{
 	},
 }
 
+var remoteRmCmd = &cobra.Command{
+	Use:   "rm [name]",
+	Short: "Remove a remote",
+	Long: `Remove a configured remote.
+
+Examples:
+  tk remote rm icloud
+`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+
+		// Load config
+		config, err := LoadConfig()
+		if err != nil {
+			return err
+		}
+
+		// Check if remote exists
+		if _, exists := config.Remotes[name]; !exists {
+			return fmt.Errorf("remote '%s' not found", name)
+		}
+
+		// Remove remote
+		delete(config.Remotes, name)
+
+		// Save config
+		if err := SaveConfig(config); err != nil {
+			return err
+		}
+
+		fmt.Printf("Removed remote '%s'\n", name)
+		return nil
+	},
+}
+
 func init() {
 	remoteCmd.AddCommand(remoteAddCmd)
 	remoteCmd.AddCommand(remoteLsCmd)
+	remoteCmd.AddCommand(remoteRmCmd)
 }
