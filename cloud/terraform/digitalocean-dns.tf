32c6b428c5f356161fb8bd85a38357fb711e2e1d
Emily April Kazak <emily@artyom.me>
2025-12-13 17:37:53 +0100

    Fix DNS to use mono server

--- cloud/terraform/digitalocean-dns.tf
+++ cloud/terraform/digitalocean-dns.tf
@@ -5,20 +5,19 @@ data "digitalocean_domain" "artyom_me" {
 
 # Wildcard DNS record for cloud subdomain
 # This allows *.cloud.artyom.me to resolve to the k3s cluster
-# Points to vm1 (control plane) which has the Ingress controller
 resource "digitalocean_record" "cloud_wildcard" {
   domain = data.digitalocean_domain.artyom_me.id
   type   = "A"
   name   = "*.cloud"
-  value  = hcloud_server.vm1.ipv4_address
+  value  = hcloud_server.mono.ipv4_address
   ttl    = 300
 }
 
-# Optional: Direct A record for cloud.artyom.me itself
+# Direct A record for cloud.artyom.me itself
 resource "digitalocean_record" "cloud_root" {
   domain = data.digitalocean_domain.artyom_me.id
   type   = "A"
   name   = "cloud"
-  value  = hcloud_server.vm1.ipv4_address
+  value  = hcloud_server.mono.ipv4_address
   ttl    = 300
 }
